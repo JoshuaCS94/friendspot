4e0791050941941c6178a36a833759d1dc00d035
Josué Cortina <joshuacs.main@gmail.com>
2022-10-26 12:55:31 -0400

    feat: Decrease vertical padding of `Button`

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -10,7 +10,7 @@ export const Button = ({
 }: ButtonProps) => (
   <button
     className={cn(
-      'rounded border px-5 py-2',
+      'rounded border px-5 py-1.5',
       disabled
         ? 'border-slate-300 bg-slate-50 text-slate-400'
         : 'border-blue-700 bg-blue-600 text-white hover:bg-blue-700 active:bg-blue-800',
