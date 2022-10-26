aa0dfa095f3d5de5cb0b9efd329a74c3686a10b4
Josué Cortina <joshuacs.main@gmail.com>
2022-10-26 12:53:21 -0400

    feat: List all friends

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 's3.amazonaws.com',
+        hostname: 'randomuser.me',
       },
       {
         protocol: 'https',
