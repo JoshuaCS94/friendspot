200608968ba6d01893b350335be322dda12cad81
Josué Cortina <joshuacs.main@gmail.com>
2022-11-13 15:31:33 -0500

    test: Add tests for `cn` utility

---
+++ src/utils/__tests__/cn.test.ts
@@ -0,0 +1,39 @@
+import { cn } from '../'
+
+describe('cn', () => {
+  it('should join all three classes separated by a space', () => {
+    const r = cn('a', 'b', 'c')
+
+    expect(r).toEqual('a b c')
+  })
+
+  it('should return the same string if only one is provided', () => {
+    const r = cn('a')
+
+    expect(r).toEqual('a')
+  })
+
+  it('should skip all falsy values', () => {
+    const r = cn('a', false, 'b', undefined, undefined, 'c', 'd', false)
+
+    expect(r).toEqual('a b c d')
+  })
+
+  it('should return empty string if all values are falsy', () => {
+    const r = cn(false, undefined, undefined, false)
+
+    expect(r).toEqual('')
+  })
+
+  it('should return empty string if no argument is provided', () => {
+    const r = cn()
+
+    expect(r).toEqual('')
+  })
+
+  it('should not trim any spaces', () => {
+    const r = cn(' a  ', 'b    ', '   c ')
+
+    expect(r).toEqual(' a   b        c ')
+  })
+})
